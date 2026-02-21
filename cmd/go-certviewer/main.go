aa89da316c2917d87d1a4d0d70a45adf0d4864a0
Martin Bergström <emarbe02@gmail.com>
2026-02-21 13:52:51 +0100

    Remove file logger

--- cmd/go-certviewer/main.go
+++ cmd/go-certviewer/main.go
@@ -3,9 +3,7 @@ package main
 import (
 	"errors"
 	"flag"
-	"io"
 	"log"
-	"os"
 
 	"github.com/mbergz/go-certviewer/internal/certfetcher"
 	"github.com/mbergz/go-certviewer/internal/certreader"
@@ -22,14 +20,6 @@ type Flags struct {
 }
 
 func main() {
-	// Setup log to also append to file
-	f, err := os.OpenFile("app.log", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
-	log.SetOutput(io.MultiWriter(f, os.Stderr))
-
 	flags := &Flags{
 		url:       flag.String("url", "", "Url of website to fetch certificate from"),
 		insecure:  flag.Bool("k", false, "Skip TLS certificate verification (allow self-signed or unknown CAs). Only used together with '-url' flag"),
